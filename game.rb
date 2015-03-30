7f7f280052adfe5943f8c8875eb7b4f5dc498f45
= <=>
2015-03-30 15:33:01 -0400

    add play and parse_input methods to game class

--- game.rb
+++ game.rb
@@ -11,12 +11,30 @@ class Game
   end
 
   def play
+    until board.game_over?
+      puts "Enter move i.e. [x, y], flag? (e.g. 1,4,true sets a flag on [1,4].
+      1,4 reveals [1,4])"
+      move = gets.chomp
+      @board.set_mark(parse_input(move))
+      render
+    end
 
+  end
+
+  def parse_input(input)
+    input = input.split(',')
+    pos = []
+    pos[0] = input[0].to_i
+    pos[1] = input[1].to_i
+    flagged = !input[2].nil?
+    [pos, flagged]
   end
 
   def render
     puts @board.display
   end
+
+
 
 
 
